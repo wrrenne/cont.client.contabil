012b607fbc40db5eb07a1af8bf29c1bb717786a8
Wilder Renne <wr.renne@outlook.com.br>
2025-11-18 07:53:28 -0300

    Activities

--- src/app/layouts/extend/activities-header/activities-header.ts
+++ src/app/layouts/extend/activities-header/activities-header.ts
@@ -1,52 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgIcon } from '@ng-icons/core';
+import { MenuModule } from 'headlessui-angular';
+import { AvatarImageComponent } from 'src/app/shared/controls/avatar-image/avatar-image';
 import { toggleAnimation } from '../../../shared/animations';
 import { ActivityPageItem } from '../../../shared/timeline/models/pagings';
-import { TimelinesService } from '../../../shared/timeline/services/timelines.service';
 import { ActivitiesNotification } from '../../../shared/timeline/notifications/activities.notification';
+import { TimelinesService } from '../../../shared/timeline/services/timelines.service';
 
 @Component({
     selector: 'activities-header',
     templateUrl: './activities-header.html',
-    host: { 'class': 'dropdown shrink-0' },
+    host: { class: 'dropdown shrink-0' },
     animations: [toggleAnimation],
-    standalone: true
+    standalone: true,
+    imports: [RouterLink, NgIcon, MenuModule, AvatarImageComponent],
 })
 export class ActivitiesHeaderComponent implements OnInit {
-
-    activities: ActivityPageItem[]
+    activities: ActivityPageItem[];
 
     hasUnread = false;
-    opened = false
+    opened = false;
 
     constructor(
         private timelinesService: TimelinesService,
-        private activitiesNotification: ActivitiesNotification
-    ) {
-    }
+        private activitiesNotification: ActivitiesNotification,
+    ) {}
 
     ngOnInit(): void {
-        this.timelinesService.ActivityReadStatusHasUnredGet().subscribe(x => {
+        this.timelinesService.ActivityReadStatusHasUnredGet().subscribe((x) => {
             if (x.obj == true) {
-                this.activitiesNotification.notifyNew()
+                this.activitiesNotification.notifyNew();
 
-                this.activitiesNotification.hasUnread$.subscribe(status => {
+                this.activitiesNotification.hasUnread$.subscribe((status) => {
                     this.hasUnread = status;
                 });
             }
-        })
+        });
     }
 
     getData() {
-        this.opened = !this.opened
+        this.opened = !this.opened;
 
         if (this.opened) {
-            this.activitiesNotification.markAsRead()
-            this.timelinesService.ActivityReadStatusUpdate().subscribe()
+            this.activitiesNotification.markAsRead();
+            this.timelinesService.ActivityReadStatusUpdate().subscribe();
 
-            this.timelinesService.activitiesByCadastroIdListGet()
-                .subscribe(x => {
-                    this.activities = x.obj
-                })
+            this.timelinesService.activitiesByCadastroIdListGet().subscribe((x) => {
+                this.activities = x.obj;
+            });
         }
     }
 }
