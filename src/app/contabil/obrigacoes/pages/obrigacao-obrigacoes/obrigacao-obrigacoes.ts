b1e3658f433544c6cb2f0c631ec8cb26b9bf324e
Wilder Renne <wr.renne@outlook.com.br>
2025-11-28 00:45:05 -0300

    Obrigacao-Obrigacoes

--- src/app/contabil/obrigacoes/pages/obrigacao-obrigacoes/obrigacao-obrigacoes.ts
+++ src/app/contabil/obrigacoes/pages/obrigacao-obrigacoes/obrigacao-obrigacoes.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
 import { ButtonDefaultComponent } from 'src/app/shared/controls/button-default/button-default';
+import { MonthButtonsComponent } from 'src/app/shared/controls/month-buttons/month-buttons';
 import { Profile2Component } from 'src/app/shared/controls/profile2/profile2';
+import { PeriodoRefreshService } from 'src/app/shared/variables/periodo-refresh.service';
 import { DateUtilsService, EncryptionService } from '../../../../shared/services';
 import { Vars } from '../../../../shared/variables';
 import { ObrigacoesParameter } from '../../../models/obrigacoes/parameters';
@@ -15,13 +17,17 @@ import { ObrigacoesService } from '../../services/obrigacoes.service';
     selector: 'obrigacao-obrigacoes-page',
     templateUrl: './obrigacao-obrigacoes.html',
     providers: [NzModalService],
-    imports: [NzTabsModule, ObrigacaoObrigacoesTableComponent, Profile2Component, ButtonDefaultComponent],
+    imports: [NzTabsModule, ObrigacaoObrigacoesTableComponent, Profile2Component, ButtonDefaultComponent, MonthButtonsComponent],
     standalone: true,
 })
 export class ObrigacaoObrigacoesPage implements OnInit {
     parameters: ObrigacoesParameter;
 
     obrigacaoId: number;
+
+    mesAtual: Date;
+
+    private periodoSubscription: Subscription;
 
     title: string;
     subTitle: string;
@@ -34,9 +40,12 @@ export class ObrigacaoObrigacoesPage implements OnInit {
         private router: Router,
         private dateUtilsService: DateUtilsService,
         private obrigacoesService: ObrigacoesService,
+        private periodoRefreshService: PeriodoRefreshService,
     ) {}
 
     ngOnInit(): void {
+        this.setMesButtons();
+
         const urlParametrs = combineLatest([this.route.params, this.route.queryParams], (params, queryParams) => ({
             ...params,
             ...queryParams,
@@ -45,13 +54,26 @@ export class ObrigacaoObrigacoesPage implements OnInit {
         urlParametrs.subscribe((r) => {
             this.obrigacaoId = this.encryptionService.decrypt(r['id']);
 
-            this.subTitle = `Vencimentos de ${this.dateUtilsService.formattedRelativeMonth(this.vars.dataInicial!)}`;
-
-            this.obrigacoesService.obrigacaoGet(this.obrigacaoId).subscribe((x) => {
-                this.title = x.obj.descricao;
-                this.parameters = { obrigacaoId: this.obrigacaoId, mesInicial: this.vars.dataInicial!, mesFinal: this.vars.dataFinal! };
-            });
+            this.getData();
         });
+
+        this.periodoSubscription = this.periodoRefreshService.refresh$.subscribe((_) => {
+            this.setMesButtons();
+            this.getData();
+        });
+    }
+
+    getData() {
+        this.subTitle = `Vencimentos de ${this.dateUtilsService.formattedRelativeMonth(this.vars.dataInicial!)}`;
+
+        this.obrigacoesService.obrigacaoGet(this.obrigacaoId).subscribe((x) => {
+            this.title = x.obj.descricao;
+            this.parameters = { obrigacaoId: this.obrigacaoId, mesInicial: this.vars.dataInicial!, mesFinal: this.vars.dataFinal! };
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.periodoSubscription) this.periodoSubscription.unsubscribe();
     }
 
     getEncryptedId(id: number): string {
@@ -60,5 +82,18 @@ export class ObrigacaoObrigacoesPage implements OnInit {
 
     cadastroClick() {
         this.router.navigate(['/sistema/obrigacoes/obrigacao', this.encryptionService.encrypt(this.obrigacaoId)]);
+    }
+
+    monthClicked(mes: Date) {
+        this.mesAtual = mes;
+
+        this.vars.periodo = {
+            dataInicial: mes,
+            dataFinal: this.dateUtilsService.lastDateOfMonth(mes),
+        };
+    }
+
+    setMesButtons() {
+        this.mesAtual = this.vars.periodo?.dataInicial!;
     }
 }
