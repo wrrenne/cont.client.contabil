2ce4d1d7e56f38a9f9232446be534ef2cedb6408
Wilder Renne <wr.renne@outlook.com.br>
2025-12-07 21:43:53 -0300

    User-novo-modal

--- src/app/contabil/users/components/user-novo-modal/user-novo-modal.ts
+++ src/app/contabil/users/components/user-novo-modal/user-novo-modal.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzModalRef } from 'ng-zorro-antd/modal';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { ButtonDefaultComponent } from 'src/app/shared/controls/button-default/button-default';
 import { InputExComponent } from 'src/app/shared/controls/input-ex/input-ex';
+import { InputTextAreaExComponent } from 'src/app/shared/controls/input-textarea-ex/input-textarea-ex';
 import { ModalBaseComponent } from 'src/app/shared/controls/modal-base/modal-base';
 import { environment } from '../../../../../environments/environment.prod';
 import { UserInput } from '../../../../shared/control/models/inputs';
@@ -16,12 +18,13 @@ import { Vars } from '../../../../shared/variables';
     selector: 'user-novo-modal',
     standalone: true,
     templateUrl: './user-novo-modal.html',
-    imports: [FormsModule, ReactiveFormsModule, ModalBaseComponent, InputExComponent, ButtonDefaultComponent],
+    imports: [FormsModule, ReactiveFormsModule, ModalBaseComponent, InputExComponent, ButtonDefaultComponent, NzCheckboxModule, InputTextAreaExComponent],
 })
 export class UserNovoModalComponent {
     showSecondForm = false;
 
     firstFormGroup: FormGroup;
+    secondFormGroup: FormGroup;
 
     constructor(
         private modal: NzModalRef,
@@ -39,24 +42,50 @@ export class UserNovoModalComponent {
 
     createForm() {
         this.firstFormGroup = this.formBuilder.group({
-            nome: [null, Validators.required],
             email: [null, Validators.required],
         });
+
+        this.secondFormGroup = this.formBuilder.group({
+            email: [null, Validators.required],
+            nome: [null, Validators.required],
+            sistemaId: [environment.sistema, Validators.required],
+            sendInvite: [false],
+        });
+        // this.firstFormGroup = this.formBuilder.group({
+        //     nome: [null, Validators.required],
+        //     email: [null, Validators.required],
+        // });
     }
 
     closeModal() {
         this.modal.close();
     }
 
+    emailPesquisar() {
+        const email = this.firstFormGroup.get('email')?.value;
+
+        this.secondFormGroup.controls['email'].setValue(email);
+
+        this.usersService.userByEmailGet(email).subscribe((x) => {
+            if (x.obj) {
+                this.secondFormGroup.controls['nome'].setValue(x.obj.userNome);
+            }
+
+            this.showSecondForm = true;
+        });
+    }
+
     submit() {
         var userInput: UserInput = {
-            nome: this.firstFormGroup.get('nome')?.value,
-            email: this.firstFormGroup.get('email')?.value,
+            nome: this.secondFormGroup.get('nome')?.value,
+            email: this.secondFormGroup.get('email')?.value,
             sistemaId: environment.sistema,
             cadastroId: this.vars.cadastro?.id!,
+            userId: this.vars.user?.id,
             master: false,
             supervisor: false,
-            sendInvite: false,
+            sendInvite: this.secondFormGroup.get('sendInvite')?.value,
+            comentario: this.secondFormGroup.get('comentario')?.value,
         };
 
         this.usersService.userCreateOrUpdate(userInput).subscribe((x) => {
@@ -66,7 +95,7 @@ export class UserNovoModalComponent {
             }
 
             this.createNotificationUserSucesso();
-            this.router.navigate(['/sistema/users/user', this.getEncryptedId(x.obj[0])]);
+            this.router.navigate(['/sistema/users/user', this.getEncryptedId(x.obj[0].id)]);
             this.closeModal();
         });
     }
